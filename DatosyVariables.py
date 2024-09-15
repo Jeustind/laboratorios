1e548084ed82d921f791536a046d78ca851f187f
Jeustind <jeustinuc@gmail.com>
2024-09-14 19:35:05 -0600

    Delete DatosyVariables.py

--- DatosyVariables.py
+++
@@ -1,8 +0,0 @@
-#Número entero que represente la edad de una persona.
-edadPersona = 17
-#Número decimal que represente la nota promedio de un estudiante.
-notaPromedio = 95.5
-#Valor booleano que indique si un estudiante ha aprobado el curso.
-aprobado = True
-#cadena de texto que contenga el nombre de un estudiante.
-nombreEstudiante = "Jeustin Ureña"
